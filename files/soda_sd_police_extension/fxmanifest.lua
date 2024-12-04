ff5c12b674427534cd35bbee404400688a22d76f
yaawm <68302393+yaawm@users.noreply.github.com>
2024-12-04 20:46:07 +0100

    added files

---
+++ files/soda_sd_police_extension/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "adamant"
+game "rdr3"
+
+author 'soda'
+description 'SD interrogation'
+version '1.0.0'
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
